da465e6ac7f97cc4c73c1c1e416fc51177eae9b2
jhellingman <jhellingman@0bfdb20c-2f20-0410-bc3a-a93b634d5059>
2008-02-29 23:47:44 +0000

    Added flushright rendering option; worked on normalizeText.pl

--- tools/normalizeText.pl
+++ tools/normalizeText.pl
@@ -1,75 +1,228 @@
-# normalize.pl -- turn HTML to canonical text to prepare a text compare
+# normalizeText.pl -- turn various formats of text to canonical text to prepare a text compare
+
+use utf8;
+binmode(STDOUT, ":utf8");
+use open ':utf8';
 
 
-use SgmlSupport qw/getAttrVal sgml2utf/;
+require Encode;
 
+use Unicode::Normalize;
+use SgmlSupport qw/getAttrVal sgml2utf_html/;
+
+
+
+
+$fileName = $ARGV[0];
+$encoding = $ARGV[1] || "latin-1";
 
 
 $tagPattern = "<(.*?)>";
 
 
-$infile = $ARGV[0];
+handleFile($fileName, $encoding);
 
 
-if ($infile =~ /\.html?$/) 
+
+sub handleFile
 {
-	normalizeHtml($infile)
+	my $fileName = shift;
+	my $encoding = shift;
 
+	my $text = readFile($fileName, $encoding);
+
+	if ($fileName =~ /\.html?$/) 
+	{
+		$text = processHtml($text);
+	}
+	elsif ($fileName =~ /\.te?xt$/) 
+	{
+		$text = processText($text);
+	}
+	elsif ($fileName =~ /\.tei$/) 
+	{
+		$text = processTei($text);
+	}
+
+	print $text;
 }
 
-sub normalizeHtml
+
+
+
+
+
+
+
+# Process a text file
+sub processText
 {
-	my $infile = shift;
-	open(INPUTFILE, $infile) || die("ERROR: Could not open input file $infile");
-	my $contents = "";
+	my $text = shift;
+	$text = normalizeSpaces($text);
+	$text = tagPars($text);
+	$text = catPars($text);
+
+	return $text;
+}
+
+
+# Process an HTML file
+sub processHtml
+{
+	my $text = shift;
+	
+	$text = normalizeSpaces($text);
+	$text = cleanHtmlTags($text);
+	$text = sgml2utf_html($text);
+
+	return $text;
+}
+
+# Process a TEI file
+sub processTei
+{
+	my $text = shift;
+	
+
+	return $text;
+}
+
+
+
+
+
+
+# Read a file in the specified encoding.
+sub readFile
+{
+	my $fileName = shift;
+	my $fileEncoding = shift;
+	my $fileContents = "";
+
+	open (INPUTFILE, $fileName) || die("Could not open input file $fileName");
+    binmode (INPUTFILE, ":encoding($fileEncoding)");
+
+	print STDERR "Opening '$fileName' with encoding '$fileEncoding'\n";
+
 	while (<INPUTFILE>)
 	{
-		$contents .= $_;
+		$fileContents .= $_;
 	}
-	close INPUTFILE;
 
-	my $remainder = $contents;
+	close (INPUTFILE);
+	return $fileContents;
+}
+
+
+
+
+
+# Concatenate paragraphs to a single line. Explicit linebreaks (<br> tags) are retained
+sub catPars()
+{
+	my $text = shift;
+
+	$text =~ s/\n/ /g;
+	$text =~ s/<p>/\n\n<p>/g;
+
+	return $text;
+}
+
+
+# Tag paragraphs in plain text. Paragraphs are blocks of text preceeded by an empty line.
+sub tagPars()
+{
+	my $text = shift;
+
+	# Mark beginning of paragraphs:
+	# $text =~ s/\n\n([A-Z0-9"'([])/\n\n<p>\1/g;
+	$text =~ s/\n\n([^\n])/\n\n<p>\1/g;
+
+	return $text;
+}
+
+
+
+# Drop almost all HTML tags, except for <p> (paragraphs), and <i>, <b>, and <sc> formatting.
+sub cleanHtmlTags
+{
+	my $text = shift;
+	my $result = "";
+
+	$text =~ s/\n/ /g;		# Get rid of all new lines.
+	$text =~ s/<style\b(.*?)>(.*?)<\/style>//g;  # Get rid of CSS.
+
+	my $remainder = $text;
 	while ($remainder =~ /$tagPattern/)
 	{
 		my $fragment = $`;
 		my $tag = $1;
 		$remainder = $';
-		handleFragment($fragment);
-		handleTag($tag);
+		$result .= handleHtmlFragment($fragment);
+		$result .= handleHtmlTag($tag);
 	}
-	handleFragment($remainder);
+	$result .= handleHtmlFragment($remainder);
+
+	$result =~ s/<p> +/<p>/g;	# Get rid of superflous spaces.
+	$result =~ s/\n +/\n/g;		# Get rid of superflous spaces.
+
+	return $result;
 }
 
-
-
-sub handleFragment
+sub handleHtmlFragment
 {
 	my $fragment = shift;
 
-    $fragment =~ s/[ \t]+/ /g;
-    $fragment =~ s/[»”„]/"/g;
-    $fragment =~ s/[’]/'/g;
-    $fragment =~ s/[…]/.../g;
+	# print STDERR "Handling fragment '$fragment'\n";
 
-	print $fragment;
+	return $fragment;
 }
 
-sub handleTag
+sub handleHtmlTag
 {
 	my $tag = shift;
 
-	# print "<$tag>\n";
+	# print STDERR "Handling tag <$tag>\n";
 
 	if ($tag =~ /^(li|td|blockquote|title|h1|h2|h3|h4|h5|h6|p)\b/i) 
 	{
-		print "\n<p>"
+		return "\n\n<p>"
 	}
 	elsif ($tag =~ /^(strong|emph|b|i)\b/i) 
 	{
-		print "<hi>"
+		return "<i>"
 	}
 	elsif ($tag =~ /^\/(strong|emph|b|i)\b/i) 
 	{
-		print "</hi>"
+		return "</i>"
 	}
+}
+
+
+
+
+
+
+# Normalize tagging of text-style tags around punctuation.
+sub normalizeTagging
+{
+
+
+}
+
+
+
+
+
+
+sub normalizeSpaces
+{
+	my $text = shift;
+
+	$text =~ s/\t/ /g;			# Tabs go to spaces
+	$text =~ s/ +/ /g;			# Series of spaces go to a single space
+	$text =~ s/ \n/\n/g;		# Spaces before new-line are removed
+	$text =~ s/\n\n+/\n\n/g;	# Multiple empty lines go to a single empty line
+
+	return $text;
 }
