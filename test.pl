e1d6e5680730b207a0e8ecd757ac28301fbbdcf9
jhellingman <jhellingman@0bfdb20c-2f20-0410-bc3a-a93b634d5059>
2009-11-13 09:59:09 +0000

    Added stylesheet to generate Dublin Core in XML.

--- test.pl
+++ test.pl
@@ -1,4 +1,4 @@
-# Perl script to test the the tei2xml.xsl stylesheet with Saxon.
+# Perl script to test the the tei2html.xsl stylesheet with Saxon.
 
 $xsldir = "C:\\Users\\Jeroen\\Documents\\eLibrary\\Tools\\tei2html";  # location of xsl stylesheets
 $saxon = "\"C:\\Program Files (x86)\\Java\\jre6\\bin\\java\" -jar C:\\Bin\\saxon\\saxon.jar "; # command to run the saxon processor (see http://saxon.sourceforge.net/, using Version 6.5.5)
@@ -29,3 +29,4 @@ if (-f "custom.css.xml")
 }
 
 system ("$saxon $basename.xml $xsldir/tei2html.xsl $fileImageParam $cssFileParam > test.html");
+system ("$saxon $basename.xml $xsldir/tei2dc.xsl  > test-dc.xml");
