9695f350670001e759c6d7a0a25891b97372c5ff
jhellingman@gmail.com <jhellingman@gmail.com@0bfdb20c-2f20-0410-bc3a-a93b634d5059>
2011-03-14 21:48:44 +0000

    Added some Tagalog translations; Started work on word-frequency list tool in XSLT.

---
+++ tei2wl.pl
@@ -0,0 +1,11 @@
+# Perl script to test the the tei2wl.xsl stylesheet with Saxon.
+
+use strict;
+
+my $xsldir = "C:\\Users\\Jeroen\\Documents\\eLibrary\\Tools\\tei2html";  # location of xsl stylesheets
+my $saxon = "\"C:\\Program Files\\Java\\jre6\\bin\\java.exe\" -jar C:\\bin\\saxonhe9\\saxon9he.jar ";
+
+my $filename = $ARGV[0];
+
+system ("$saxon $filename $xsldir/tei2wl.xsl");
+
