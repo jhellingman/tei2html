86a52bc7292de019791da4814e5c89ad91f2b639
jhellingman@gmail.com <jhellingman@gmail.com@0bfdb20c-2f20-0410-bc3a-a93b634d5059>
2011-10-07 22:40:42 +0000

    Renamed pseudoh# classes to h# classes; no longer use # in generated figure ids.

--- trunk/tools/pgpp.pl
+++ trunk/tools/pgpp.pl
@@ -1,16 +1,17 @@
 # pgpp.pl
 
+use strict;
 use SgmlSupport qw/pgdp2sgml/;
 
 
-$file = $ARGV[0];
-$useExtensions = 0;
+my $file = $ARGV[0];
+my $useExtensions = 0;
 
-if ($file eq "-x") 
+if ($file eq "-x")
 {
-	$useExtensions = 1;
-	$file = $ARGV[1];
-	print STDERR "USING EXTENSIONS FOR FRANCK!\n";
+    $useExtensions = 1;
+    $file = $ARGV[1];
+    print STDERR "USING EXTENSIONS FOR FRANCK!\n";
 }
 
 
@@ -19,49 +20,49 @@ open(INPUTFILE, $file) || die("Could not open input file $file");
 
 while (<INPUTFILE>)
 {
-	# Replace ampersands (if they are not likely entities):
-	$_ =~ s/\& /\&amp; /g;
-	$_ =~ s/\&$/\&amp;/g;
-	$_ =~ s/\&c\. /\&amp;c. /g;
+    # Replace ampersands (if they are not likely entities):
+    $_ =~ s/\& /\&amp; /g;
+    $_ =~ s/\&$/\&amp;/g;
+    $_ =~ s/\&c\. /\&amp;c. /g;
 
-	# Replace PGDP page-separators (preserving proofers):
-	# $_ =~ s/^-*File: 0*([0-9]+)\.png-*\\([^\\]+)\\([^\\]+)\\([^\\]+)\\([^\\]+)\\.*$/<pb n=\1 resp="\2|\3|\4|\5">/g;
-	$_ =~ s/^-*File: 0*([0-9]+)\.png-*\\([^\\]+)\\([^\\]?)\\([^\\]+)\\([^\\]+)\\.*$/<pb n=\1>/g;
-	# For DP-EU:
-	$_ =~ s/^-*File: 0*([0-9]+)\.png-*\\([^\\]+)\\([^\\]+)\\.*$/<pb n=\1>/g;
-	# For omitted proofer names.
-	$_ =~ s/^-*File: 0*([0-9]+)\.png-*$/<pb n=\1>/g;
+    # Replace PGDP page-separators (preserving proofers):
+    # $_ =~ s/^-*File: 0*([0-9]+)\.png-*\\([^\\]+)\\([^\\]+)\\([^\\]+)\\([^\\]+)\\.*$/<pb n=\1 resp="\2|\3|\4|\5">/g;
+    $_ =~ s/^-*File: 0*([0-9]+)\.png-*\\([^\\]+)\\([^\\]?)\\([^\\]+)\\([^\\]+)\\.*$/<pb n=\1>/g;
+    # For DP-EU:
+    $_ =~ s/^-*File: 0*([0-9]+)\.png-*\\([^\\]+)\\([^\\]+)\\.*$/<pb n=\1>/g;
+    # For omitted proofer names.
+    $_ =~ s/^-*File: 0*([0-9]+)\.png-*$/<pb n=\1>/g;
 
-	# Replace footnote indicators:
-	$_ =~ s/\[([0-9]+)\]/<note n=\1><\/note>/g;
+    # Replace footnote indicators:
+    $_ =~ s/\[([0-9]+)\]/<note n=\1><\/note>/g;
 
-	# Mark beginning of paragraphs:
-	$_ =~ s/\n\n([A-Z0-9"'([])/\n\n<p>\1/g;
+    # Mark beginning of paragraphs:
+    $_ =~ s/\n\n([A-Z0-9"'([])/\n\n<p>\1/g;
 
-	# Replace illustration markup:
-	$_ =~ s/\[Illustration: (.*)\]/<figure id=p#>\n<head>\1<\/head>\n<\/figure>/g;
+    # Replace illustration markup:
+    $_ =~ s/\[Illustration: (.*)\]/<figure id=pXXX>\n<head>\1<\/head>\n<\/figure>/g;
 
-	# Replace dashes in number ranges by en-dashes
-	$_ =~ s/([0-9])-([0-9])/\1\&ndash;\2/g;
+    # Replace dashes in number ranges by en-dashes
+    $_ =~ s/([0-9])-([0-9])/\1\&ndash;\2/g;
 
-	# Replace two dashes by em-dashes
-	$_ =~ s/--/\&mdash;/g;
+    # Replace two dashes by em-dashes
+    $_ =~ s/--/\&mdash;/g;
 
-	# Replace superscripts
-	$_ =~ s/\^\{(.*?)\}/<hi rend=sup>\1<\/hi>/g;
-	$_ =~ s/\^([a-zA-Z0-9\*])/<hi rend=sup>\1<\/hi>/g;
+    # Replace superscripts
+    $_ =~ s/\^\{(.*?)\}/<hi rend=sup>\1<\/hi>/g;
+    $_ =~ s/\^([a-zA-Z0-9\*])/<hi rend=sup>\1<\/hi>/g;
 
-	# Replace subscripts
-	$_ =~ s/_\{(.*?)\}/<hi rend=sub>\1<\/hi>/g;
-	$_ =~ s/_([a-zA-Z0-9\*])/<hi rend=sub>\1<\/hi>/g;
+    # Replace subscripts
+    $_ =~ s/_\{(.*?)\}/<hi rend=sub>\1<\/hi>/g;
+    $_ =~ s/_([a-zA-Z0-9\*])/<hi rend=sub>\1<\/hi>/g;
 
-	# Replace italics tag
-	$_ =~ s/<i>/<hi>/g;
-	$_ =~ s/<\/i>/<\/hi>/g;
+    # Replace italics tag
+    $_ =~ s/<i>/<hi>/g;
+    $_ =~ s/<\/i>/<\/hi>/g;
 
-	# Replace special accented letters
-	# $_ =~ s/\[=([aieouAIEOU])\]/\&\1macr;/g;
-	$_ = pgdp2sgml($_, $useExtensions);
+    # Replace special accented letters
+    # $_ =~ s/\[=([aieouAIEOU])\]/\&\1macr;/g;
+    $_ = pgdp2sgml($_, $useExtensions);
 
-	print;
+    print;
 }
