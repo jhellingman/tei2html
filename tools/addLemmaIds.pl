717425bed79956bea0b9d5dfffbc132cc5121617
jhellingman@gmail.com <jhellingman@gmail.com@0bfdb20c-2f20-0410-bc3a-a93b634d5059>
2012-01-31 22:27:26 +0000

    Added a small script to add line number and column info to an SGML/XML file, to improve error reporting (my XSLT processor does not report line-numbers, this is the easiest way to add those.)

--- tools/addLemmaIds.pl
+++ tools/addLemmaIds.pl
@@ -18,8 +18,8 @@
 #
 # And references to be replaced as follows:
 #
-#		 z. lemma -> z. <ref target=lemma>lemma</a>
-#		 z. lemma No 2.-> z. <ref target=lemma2>lemma No 2.</a>
+#        z. lemma -> z. <ref target=lemma>lemma</a>
+#        z. lemma No 2.-> z. <ref target=lemma2>lemma No 2.</a>
 
 
 %lemmaHash = ();
@@ -42,37 +42,37 @@ $patCrossReference = /zie (men|ook|verder|echter onder) ([a-z]+)( en ([a-z]+))/;
 
 sub createIds
 {
-	# find paragraphs starting with bold words and comma
-	# extract lemma
-	# insert id tag.
+    # find paragraphs starting with bold words and comma
+    # extract lemma
+    # insert id tag.
 
-	# Special case: Greek lemma (not bold)
-	# - just use greek letters in ID.
+    # Special case: Greek lemma (not bold)
+    # - just use greek letters in ID.
 
-	# Special case: multiple meanings (numbered 1) 2) 3)
-	# - tag the numbers with <ab> and numbered id. (lemma_1, lemma_2, etc.) (register collision on lemma)
+    # Special case: multiple meanings (numbered 1) 2) 3)
+    # - tag the numbers with <ab> and numbered id. (lemma_1, lemma_2, etc.) (register collision on lemma)
 
 }
 
 
 sub createRefs
 {
-	# find potential cross references (words in italics, preceeded with "zie" or something similar.
-	# lookup whether we have an id for it.
-	# link to it.
+    # find potential cross references (words in italics, preceeded with "zie" or something similar.
+    # lookup whether we have an id for it.
+    # link to it.
 
-	# special case: collision:
-	# - handle by adding reference to lemma_X. This will need to be manually resolved.
+    # special case: collision:
+    # - handle by adding reference to lemma_X. This will need to be manually resolved.
 
 }
 
 
 sub createId
 {
-	# strip diacritics
-	# replace spaces with underscores
-	# fold to lowercase
-	# check for possible collision (add letter to end to avoid collision)  (lemma_a, lemma_b, etc.) (register collision on lemma)
+    # strip diacritics
+    # replace spaces with underscores
+    # fold to lowercase
+    # check for possible collision (add letter to end to avoid collision)  (lemma_a, lemma_b, etc.) (register collision on lemma)
 
 }
 
---
+++ tools/addPositionInfo.pl
@@ -0,0 +1,35 @@
+# addPositionInfo.pl -- add linenumber and column info to SGML/XML files
+
+use strict;
+
+my $inputFile = $ARGV[0];
+
+open(INPUTFILE, $inputFile) || die("Could not open $inputFile");
+
+print STDERR "Adding __pos attributes to $inputFile\n";
+
+my $lineNumber = 0;
+
+while (<INPUTFILE>)
+{
+    my $remainder = $_;
+    $lineNumber++;
+    my $column = 0;
+
+    while ($remainder =~ m/<([a-z][a-z0-9._-]*)(.*?)>/i)
+    {
+        my $before = $`;
+        my $tag = $1;
+        my $attrs = $2;
+        $remainder = $';
+        $column += length "$before";
+
+        $attrs = " __pos='$lineNumber:$column'$attrs";
+        print "$before<$tag$attrs>";
+
+        # Do not count added attribute, as it is not in the source.
+        $column += length "<$tag$attrs>";
+    }
+    print $remainder;
+}
+
