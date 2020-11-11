44ecda5cdbf367a362d18c55893a58662e1243e6
Jeroen Hellingman <jeroen@bohol.ph>
2020-11-11 22:59:55 +0100

    Improve script fixPb.pl to deal with split double columns.

--- tools/fixPb.pl
+++ tools/fixPb.pl
@@ -1,4 +1,4 @@
-# fixPb.pl -- fix (renumber) page breaks in TEI tagged files
+# fixPb.pl -- fix (renumber) page-breaks in a TEI file.
 #
 # 1. sequence of numbers
 # 2. average size of pages with large deviations
@@ -6,13 +6,56 @@
 use strict;
 use warnings;
 
-use Roman;                          # Roman.pm version 1.1 by OZAWA Sakuro <ozawa@aisoft.co.jp>
+use Roman;              # Roman.pm version 1.1 by OZAWA Sakuro <ozawa@aisoft.co.jp>
+use Getopt::Long;
+
 use SgmlSupport qw/getAttrVal/;
 
+my $splitColumns = 0;
+my $useRoman = 0;
+my $first = 0;
+my $last = 0;
+my $offset = 0;
+my $new = 0;
+my $showHelp = 0;
+
+GetOptions(
+    's' => \$splitColumns,
+    'r' => \$useRoman,
+    'f=i' => \$first,
+    'l=i' => \$last,
+    'o=i' => \$offset,
+    'n=i' => \$new,
+    'help' => \$showHelp,
+    );
+
 my $inputFile   = $ARGV[0];
-my $firstChange = $ARGV[1];
-my $offset      = $ARGV[2];
-my $useRoman    = defined $ARGV[3] ? $ARGV[3] : '';
+
+if ($new != 0) {
+    $offset = -($first - $new);
+}
+
+if ($showHelp == 1 or $offset == 0) {
+    my $help = <<'END_HELP';
+
+fixPb.pl -- fix (renumber) page-breaks in a TEI file.
+
+Usage: fixPb.pl [-srfloi] <inputfile.tei>
+
+Options:
+    s         Deal with split columns. Every second <pb> is replaced with <cb>. 
+                After each page, the offset will be decreased by one, to restore
+                the original page-numbering.
+    r         Use Roman numerals.
+    f=<int>   First page to adjust (based on @n attribute).
+    l=<int>   Last page (this last page will not be adjusted).
+    o=<int>   Offset, will be added to the original page number.
+    n=<int>   New number of first page; will be used to calculate the offset.
+END_HELP
+
+    print $help;
+    exit 0;
+}
 
 my $previousPage = 0;
 my $currentPage = 0;
@@ -21,6 +64,8 @@ my $totalPageSize = 0;
 my $numberOfPages = 1;
 
 my $seenFirst = 0;
+my $seenLast = 0;
+my $secondColumn = 0;
 
 open(INPUTFILE, $inputFile) || die("Could not open $inputFile");
 
@@ -45,21 +90,41 @@ while (<INPUTFILE>) {
         my $cp = isroman($currentPage) ? arabic($currentPage) : $currentPage;
         my $pp = isroman($previousPage) ? arabic($previousPage) : $previousPage;
 
-        if (isnum($currentPage) && $cp == $firstChange) {
+        if (isnum($currentPage) && $cp == $first) {
             $seenFirst = 1;
         }
-        if ($seenFirst && isnum($currentPage)) {
-            my $newCurrentPage = $currentPage + $offset;
-            if ($useRoman eq 'R') {
-                $newCurrentPage = uc(roman($newCurrentPage));
-            }
-            print "$before<pb id=pb$newCurrentPage n=$newCurrentPage$facs>";
+        if (isnum($currentPage) && $cp == $last) {
+            $seenLast = 1;
+        }
+        if ($seenFirst && !$seenLast && isnum($currentPage)) {
+            print $before;
+            print nextPageBreak($currentPage, $facs);
         } else {
             print "$before<pb$attrs>";
         }
     }
     $currentPageSize += length $remainder;
     print $remainder;
+}
+
+
+sub nextPageBreak {
+    my $currentPage = shift;
+    my $facs = shift;
+
+    if ($splitColumns and $secondColumn) {
+        $secondColumn = 0;
+        $offset -= 1;
+        return "<cb>";
+    }
+    else {
+        my $newCurrentPage = $currentPage + $offset;
+        if ($useRoman eq 'R') {
+            $newCurrentPage = uc(roman($newCurrentPage));
+        }
+        $secondColumn = 1;
+        return "<pb id=pb$newCurrentPage n=$newCurrentPage$facs>";
+    }
 }
 
 
