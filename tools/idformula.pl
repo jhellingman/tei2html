cd2dd95a429c4b9fe11835c50927a703ed10f5b4
Jeroen Hellingman <jeroen@bohol.ph>
2021-03-06 22:24:26 +0100

    Improve idformula perl script.

--- tools/idformula.pl
+++ tools/idformula.pl
@@ -1,12 +1,21 @@
-# idFormula.pl -- give formula-elements ids based on de page and number they have
+# idFormula.pl -- give formula-elements ids based on de page they are on and a sequence number.
 
 use strict;
 use warnings;
+
+use Getopt::Long;
 use SgmlSupport qw/getAttrVal/;
+
+my $idAll = 0;      # Give a new id to all formulas, not just those who don't have one.
+my $prefix = 'f';   # Prefix to use for a new id: format will be <prefix><page number>.<number on page>
+
+GetOptions(
+    'a' => \$idAll,
+    'p=s' => \$prefix);
 
 my $inputFile = $ARGV[0];
 my $pageNumber = 0;
-my $lgNumber = 0;
+my $formulaNumber = 0;
 
 open(INPUTFILE, $inputFile) || die("Could not open $inputFile");
 
@@ -21,14 +30,14 @@ while (<INPUTFILE>) {
         $remainder = $';
 
         idElement($before, $pageNumber);
-        $lgNumber = 0;
+        $formulaNumber = 0;
         $pageNumber = getAttrVal('n', $pbAttrs);
         print $pbTag;
     }
     idElement($remainder, $pageNumber);
 }
 
-sub idElement($$) {
+sub idElement {
     my $remainder = shift;
     my $pageNumber = shift;
 
@@ -40,9 +49,14 @@ sub idElement($$) {
 
         my $id = getAttrVal('id', $attrs);
         my $newTag = $tag;
-        if ($id eq '' || $id eq 'fXXX') {
-            $lgNumber++;
-            $newTag = "<formula id=f$pageNumber.$lgNumber$attrs>";
+
+        if ($idAll == 1) {
+            $formulaNumber++;
+            my $newAttrs = stripAttribute('id', $attrs);
+            $newTag = "<formula id=$prefix$pageNumber.$formulaNumber$newAttrs>";
+        } elsif ($id eq '' || $id eq 'fXXX') {
+            $formulaNumber++;
+            $newTag = "<formula id=$prefix$pageNumber.$formulaNumber$attrs>";
         }
 
         print STDERR "$tag    ->    $newTag\n";
@@ -50,4 +64,17 @@ sub idElement($$) {
         print $newTag;
     }
     print $remainder;
+}
+
+sub stripAttribute {
+    my $attrName = shift;
+    my $attrs = shift;
+
+    if ($attrs =~ /$attrName\s*=\s*([\w.-]+)/i) {
+        $attrs =~ s/$attrName\s*=\s*([\w.-]+)//i;
+    } elsif ($attrs =~ /$attrName\s*=\s*\"(.*?)\"/i) {
+        $attrs =~ s/$attrName\s*=\s*\"(.*?)\"//i;
+    }
+    $attrs =~ s/\s+/ /;
+    return $attrs;
 }
