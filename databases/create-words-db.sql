e581da0dcebef77e6b338272193644a63900e3d6
Jeroen Hellingman <jeroen@bohol.ph>
2023-01-10 23:12:52 +0100

    Export database structure for words database; some useful queries.

--- databases/create-words-db.sql
+++ databases/create-words-db.sql
@@ -1,46 +1,63 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `words` DEFAULT CHARACTER SET utf8 COLLATE default collation ;
-USE `words`;
+CREATE DATABASE `words` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 
--- -----------------------------------------------------
--- Table `words`.`book`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `words`.`Book` (
-  `idbook` INT NOT NULL AUTO_INCREMENT ,
-  `title` VARCHAR(128) NULL ,
-  `author` VARCHAR(128) NULL ,
-  `file` VARCHAR(128) NULL ,
-  PRIMARY KEY (`idbook`) ,
-  INDEX `title` (`title` ASC) ,
-  INDEX `author` (`author` ASC) )
-ENGINE = InnoDB;
+-- words.book definition
+
+CREATE TABLE `book` (
+  `idbook` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(768) DEFAULT NULL,
+  `author` varchar(768) DEFAULT NULL,
+  `file` varchar(128) DEFAULT NULL,
+  `changeDate` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`idbook`),
+  KEY `title` (`title`),
+  KEY `author` (`author`)
+) ENGINE=InnoDB AUTO_INCREMENT=69749 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
--- -----------------------------------------------------
--- Table `words`.`word`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `words`.`Word` (
-  `idword` INT NOT NULL AUTO_INCREMENT ,
-  `idbook` INT NULL ,
-  `word` VARCHAR(128) NULL ,
-  `canonical` VARCHAR(128) NULL,
-  `language` VARCHAR(8) NULL ,
-  `count` INT NULL ,
-  PRIMARY KEY (`idword`) ,
-  INDEX `word` (`word` ASC, `language` ASC) ,
-  INDEX `idbook` (`idbook` ASC) ,
-  CONSTRAINT `idbook`
-    FOREIGN KEY (`idbook` )
-    REFERENCES `words`.`Book` (`idbook` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- words.word definition
+
+CREATE TABLE `word` (
+  `idword` int(11) NOT NULL AUTO_INCREMENT,
+  `idbook` int(11) DEFAULT NULL,
+  `word` varchar(128) DEFAULT NULL,
+  `canonical` varchar(128) DEFAULT NULL,
+  `language` varchar(32) DEFAULT NULL,
+  `count` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idword`),
+  KEY `word` (`word`,`language`),
+  KEY `idbook` (`idbook`),
+  CONSTRAINT `idbook` FOREIGN KEY (`idbook`) REFERENCES `book` (`idbook`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=10153301 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
+-- words.title definition
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE `title` (
+  `idbook` int(11) NOT NULL,
+  `language` varchar(100) DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `nfc` int(11) DEFAULT 0,
+  `title` varchar(512) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- words.person definition
+
+CREATE TABLE `person` (
+  `idbook` int(11) DEFAULT NULL,
+  `key` varchar(512) DEFAULT NULL,
+  `name` varchar(512) DEFAULT NULL,
+  `viaf` varchar(100) DEFAULT NULL,
+  `role` varchar(100) DEFAULT NULL,
+  `dates` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- words.keyword definition
+
+CREATE TABLE `keyword` (
+  `idbook` int(11) DEFAULT NULL,
+  `keyword` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
